36794d1b3fa45aa2b7c427d955cc42e90cfdbeae
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-17 19:00:14 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Quiz />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './ActiveQuiz.css';
+import AnswersList from './AnswersList/AnswersList';
+
+const ActiveQuiz = (props) => (
+  <div className={classes.ActiveQuiz}>
+    <p className={classes.Question}>
+      <span>
+        <strong>2.</strong>&nbsp; test
+      </span>
+
+      <small>4 из 12</small>
+    </p>
+    <AnswersList answers={props.answers} />
+  </div>
+);
+
+export default ActiveQuiz;
