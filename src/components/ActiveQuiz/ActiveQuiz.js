9913d298e80b8cf340bad38589babfd78a3aaa3d
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-18 14:11:50 +0300

    finished studying components

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -6,12 +6,18 @@ const ActiveQuiz = (props) => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <strong>2.</strong>&nbsp; test
+        <strong>{props.answerNumber}.</strong>&nbsp; {props.question}
       </span>
 
-      <small>4 из 12</small>
+      <small>
+        {props.answerNumber} из {props.quizLength}
+      </small>
     </p>
-    <AnswersList answers={props.answers} />
+    <AnswersList
+      answers={props.answers}
+      onAnswerClick={props.onAnswerClick}
+      state={props.state}
+    />
   </div>
 );
 
