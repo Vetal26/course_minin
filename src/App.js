469b6bfc1d04ffe554dd51a835867188fbab8c78
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-11-18 15:28:28 +0300

    finished studying routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import Layout from './hoc/Layout/Layout';
 import Quiz from './containers/Quiz/Quiz';
+import { Route, Routes } from 'react-router-dom';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
+import Auth from './containers/Auth/Auth';
+import QuizList from './containers/QuizList/QuizList';
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Routes>
+        <Route path="auth" element={<Auth />} />
+        <Route path="quiz-creator" element={<QuizCreator />} />
+        <Route path="quiz/:id" element={<Quiz />} />
+        <Route path="/" element={<QuizList />} />
+      </Routes>
     </Layout>
   );
 }
--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import classes from './Drawer.css';
+import { NavLink } from 'react-router-dom';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const links = [1, 2, 3];
+const links = [
+  { to: '/', label: 'Список', end: true },
+  { to: '/auth', label: 'Авторизация', end: false },
+  { to: '/quiz-creator', label: 'Создать тест', end: false },
+];
 
 class Drawer extends React.Component {
+  clickHandler = () => {
+    this.props.onClose();
+  };
+
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            end={link.end}
+            className={({ isActive }) => (isActive ? classes.active : '')}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
       );
     });
